21f4577e8ffe667470aad3ef417b15a91055c99c
John Ankarström <john@ankarstrom.se>
2024-05-04 18:53:04 +0200

    Add comments.

--- init.lua
+++ init.lua
@@ -32,85 +32,114 @@ function ScrollDesktop:start()
   self.currentWindows = nil
   self.positions = {}
   self.xmax = hs.screen.mainScreen():fullFrame().w
+
   self.tap = hs.eventtap.new({scrollWheel}, function(event)
-    local dx = event:getProperty(scrollWheelEventPointDeltaAxis2)
-    local begin = event:getProperty(scrollWheelEventScrollPhase) == 1
-    if begin then
+    local beginEvent = event:getProperty(scrollWheelEventScrollPhase) == 1
+
+    -- At the beginning of the scroll, determine the type of scroll.
+    if beginEvent then
       local window = get_window_under_mouse()
       local mod = hs.eventtap.checkKeyboardModifiers()
+
+      -- Scroll windows only if pointer is not above window or Cmd is held.
       self.triggerSwipe = window == nil or mod.cmd
+
+      -- Exempt window under pointer from scroll if Shift is held.
       if window ~= nil and mod.shift then
         window:focus()
         self.exemptWindow = window:id()
       else
         self.exemptWindow = nil
       end
+
+      -- Scroll only window under pointer if Option is held.
       if window ~= nil and mod.alt then
         window:focus()
         self.onlyWindow = window
       else
         self.onlyWindow = nil
       end
+
+      -- Scroll only windows to the right of pointer if Ctrl is held.
       if mod.ctrl then
         self.onlyRightOf = hs.mouse:getRelativePosition().x
       else
         self.onlyRightOf = nil
       end
-    end
-    if self.triggerSwipe then
-      if begin then
+
+      -- Collect windows to be scrolled.
+      if self.triggerSwipe then
         if self.onlyWindow == nil then
           self.currentWindows = hs.window.orderedWindows()
         else
           self.currentWindows = {self.onlyWindow}
         end
       end
+    end
+
+    -- Scroll windows.
+    if self.triggerSwipe then
       for i, window in pairs(self.currentWindows) do
         local id = window:id()
+        local dx = event:getProperty(scrollWheelEventPointDeltaAxis2)
+
+        -- Don't scroll exempt window.
         if id ~= self.exemptWindow then
+          -- Get real or virtual position of window.
           local topleft = self.positions[id]
           if self.positions[id] == nil then
             topleft = window:topLeft()
           end
-          local allow = true
+
+          -- Don't scroll windows to the left if Ctrl is held.
+          local isRight = true
           local x = topleft.x+dx
           if self.onlyRightOf ~= nil then
             local diff = topleft.x-self.onlyRightOf
             if diff < 0 or diff == 0 and dx < 0 then
-              allow = false
+              isRight = false
             end
             if topleft.x+dx <= self.onlyRightOf then
               x = self.onlyRightOf+1
             end
           end
-          if allow then
-            local outside = false
+
+          if isRight then
+            -- If window is at screen edge ("outside"), then use
+            -- virtual instead of real positions.
+            local isOutside = false
             if x > self.xmax-1 then
               self.positions[id] = {x=topleft.x+dx, y=topleft.y}
               x = self.xmax-1
-              outside = true
+              isOutside = true
             else
               local minx = -window:size().w
               if x < minx+1 then
                 self.positions[id] = {x=topleft.x+dx, y=topleft.y}
                 x = minx+1
-                outside = true
+                isOutside = true
               end
             end
+
+            -- Move pointer with window if Option is held.
             if self.onlyWindow ~= nil then
               local pos = hs.mouse.getRelativePosition()
               pos.x = pos.x+x-window:topLeft().x
               hs.mouse.setRelativePosition(pos)
             end
+
+            -- Set real window position.
             window:setTopLeft(x, topleft.y)
-            if not outside then self.positions[id] = nil end
+
+            -- Remove virtual window position if window is "inside".
+            if not isOutside then self.positions[id] = nil end
           end
         end
       end
       return true
     end
-  end
-  ):start()
+  end)
+  self.tap:start()
 end
 
 function ScrollDesktop:stop()
